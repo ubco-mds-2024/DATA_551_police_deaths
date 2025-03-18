e58419d16fb0e92a676af337445dca81bf4c6e1b
jiakunli <102328451+Kunnn12@users.noreply.github.com>
2025-03-17 19:41:35 -0700

    Update demo.py

--- src/demo.py
+++ src/demo.py
@@ -660,4 +660,3 @@ def update_year_display(year_range):
 server = app.server  
 if __name__ == '__main__':
     app.run_server(debug=False, host="0.0.0.0", port=8050)
-    app.run(debug=True, use_reloader=False)
